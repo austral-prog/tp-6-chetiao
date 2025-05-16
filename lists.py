c13f786ac9dbc6b3ad000b6ace67ba9ea36f9b03
Tiago Farach <tnfarach@mail.austral.edu.ar>
2025-05-15 22:35:38 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,62 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    lista = list_to_remove_elements
+
+    lista_corta = lista[1:4]
+    
+    return lista_corta
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    lista = list_to_add_elements
+    lista = ['Pink'] + lista + ['Yellow']
+
+    return lista
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    lista = list_to_check
 
+    if len(lista) == 0:
+        return True
+    else:
+        return False
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    l1 = list_to_compare1
+    l2 = list_to_compare2
+
+    if len(l1) > 2 and len(l2) > 2 and l1[2] == l2[2]:
+        return True
+    else:
+        return False
+    
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    modified_list = []
+    first_list = list_of_lists_to_modify[0]
+    second_list = list_of_lists_to_modify[1]
+    third_list = list_of_lists_to_modify[2]
+
+    modified_list.append(first_list[:2])
+
+    if len(second_list) >= 4:
+        modified_list.append(second_list[1:4])
+    elif len(second_list) == 3:
+        modified_list.append(second_list[1:3])
+    elif len(second_list) == 2:
+        modified_list.append(second_list[1:])
+    else:
+        modified_list.append([])
+
+    modified_list.append(third_list[-2:])
+
+    return modified_list
+
+
